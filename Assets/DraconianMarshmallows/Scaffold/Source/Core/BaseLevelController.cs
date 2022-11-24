f90087ffcac83ae4c04521af01156f07b89fe6da
scarypurple <purple.scaredhaunt@gmail.com>
2022-11-24 14:44:05 -0700

    Add base main & level controller stub classes.

---
+++ Assets/DraconianMarshmallows/Scaffold/Source/Core/BaseLevelController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace DraconianMarshmallows.Core
+{
+  public class BaseLevelController : MonoBehaviour
+  {
+    
+  }
+}
---
+++ Assets/DraconianMarshmallows/Scaffold/Source/Core/BaseMainController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace DraconianMarshmallows.Core
+{
+  public class BaseMainController : MonoBehaviour
+  {
+    
+  }
+}
