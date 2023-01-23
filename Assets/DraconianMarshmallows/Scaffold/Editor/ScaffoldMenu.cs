1cc93e4f5becafdc96813153ce770c023062c59b
scarypurple <purple.scaredhaunt@gmail.com>
2023-01-22 19:02:01 -0700

    Add menu item to set scenes to be loaded for testing.

--- Assets/DraconianMarshmallows/Scaffold/Editor/ScaffoldMenu.cs
+++ Assets/DraconianMarshmallows/Scaffold/Editor/ScaffoldMenu.cs
@@ -1,5 +1,7 @@
+using DraconianMarshmallows.Core;
 using JetBrains.Annotations;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace DraconianMarshmallows.Scaffold.Editor
@@ -9,9 +11,27 @@ namespace DraconianMarshmallows.Scaffold.Editor
     [MenuItem("Draconian Marshmallows/Set Current Scenes as Test Level")][UsedImplicitly]
     public static void SetCurrentScenesAsTestLevelScenes()
     {
-      Debug.Log("Doing stuff...");
-      
-      
+      var assetIds = AssetDatabase.FindAssets("LevelData t:LevelData", new []{"Assets/Data"});
+      var dataPath = AssetDatabase.GUIDToAssetPath(assetIds[0]);
+      var data = AssetDatabase.LoadAssetAtPath<LevelData>(dataPath);
+      Debug.Log("Initial level index: " + data.initialLevelSceneIndex);
+
+      var sceneCount = EditorSceneManager.sceneCount;
+      Debug.Log("Scenes loaded: " + sceneCount);
+      if (sceneCount < 2)
+      {
+        Debug.LogError("Please add your main(persistent) & first level scenes to the hierarchy " +
+                       "with the first scene being your main scene.");
+        return;
+      }
+
+      data.initialLevelSceneIndex = EditorSceneManager.GetSceneAt(1).buildIndex;
+      AssetDatabase.ForceReserializeAssets(new []{dataPath});
+//      AssetDatabase.ImportAsset(dataPath);
+      Debug.Log("Initial level index updated to: " + data.initialLevelSceneIndex);
+
+      EditorSceneManager.playModeStartScene 
+        = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorSceneManager.GetSceneAt(0).path);
     }
   }
 }
